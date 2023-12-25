d3b1ce4ea4c15c530beb350b2cf6fd6af673ac3b
Emmanuel Ojighoro <awesomeemoj@gmail.com>
2023-12-25 05:35:22 +0100

    Initialize NLP API with Python-flask and the transformers library; Set up docker containerization

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from transformers import pipeline
+
 app = Flask(__name__)
+pipe = pipeline("question-answering", model="deepset/tinyroberta-squad2")
 
 @app.route('/')
 def hello_world():
     return 'Hello, Docker!'
+
+@app.route('/qa', methods=['POST'])
+def question_answering():
+    data = request.json
+    question = data.get("question")
+    context = data.get("context")
+    result = pipe(question=question, context=context)
+    return jsonify(result)
+
